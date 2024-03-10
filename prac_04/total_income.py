92e55192ccf33f089b989e38cb26a2eccae3c244
maingin <maingin189@gmail.com>
2024-03-10 22:16:26 +0800

    Add prac 4 files

---
+++ prac_04/total_income.py
@@ -0,0 +1,30 @@
+"""
+CP1404/CP5632 Practical
+Starter code for cumulative total income program
+"""
+
+
+def main():
+    """Display income report for incomes over a given number of months."""
+    incomes = []
+    months = int(input("How many months? "))
+
+    for month in range(1, months + 1):
+        income = float(input(f"Enter income for month {str(month)}: "))
+        # income = float(input("Enter income for month " + str(month) + ": "))
+        incomes.append(income)
+
+    print_report(incomes, months)
+
+
+def print_report(incomes, months):
+    print("\nIncome Report\n-------------")
+    total = 0
+    for month in range(1, months + 1):
+        income = incomes[month - 1]
+        total += income
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total: 10.2f}")
+        # print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
